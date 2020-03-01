7580990450eba8f1278a7a590b0ade7e12cb7a9c
Windesson Almeida <silvapro@Windessons-MBP.attlocal.net>
2020-03-01 08:41:46 -0500

    complete react authentication

--- client/src/App.js
+++ client/src/App.js
@@ -15,20 +15,27 @@ import Authenticated from './components/Authenticated';
 
 // New import
 import withContext from './Context';
+import PrivateRoute from './PrivateRoute';
 
+const HeaderWithContext = withContext(Header);
 const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const AuthWithContext = withContext(Authenticated);
+const UserSignOutWithContext = withContext(UserSignOut);
+
 
 export default () => (
   <Router>
     <div>
-      <Header />
+      <HeaderWithContext />
 
       <Switch>
         <Route exact path="/" component={Public} />
-        <Route path="/authenticated" component={Authenticated} />
-        <Route path="/signin" component={UserSignIn} />
+        <PrivateRoute path="/authenticated" component={AuthWithContext} />
+        <PrivateRoute path="/settings" component={AuthWithContext} />
+        <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
-        <Route path="/signout" component={UserSignOut} />
+        <Route path="/signout" component={UserSignOutWithContext} />
         <Route component={NotFound} />
       </Switch>
     </div>
--- client/src/Context.js
+++ client/src/Context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cookies from 'js-cookie';
 import Data from './Data';
 
 const Context = React.createContext(); 
@@ -10,9 +11,20 @@ export class Provider extends Component {
     this.data = new Data();
   }
 
+  state = {
+    authenticatedUser: Cookies.getJSON('authenticatedUser') || null
+  };
+
   render() {
+    const { authenticatedUser } = this.state;
+
     const value = {
+      authenticatedUser,
       data: this.data,
+      actions: {
+        signIn: this.signIn,
+        signOut: this.signOut
+       }
     };
 
     return (
@@ -23,12 +35,27 @@ export class Provider extends Component {
   }
 
   
-  signIn = async () => {
-
+  signIn = async (username, password) => {
+    const user = await this.data.getUser(username, password);
+    if (user !== null) {
+      this.setState(() => {
+        return {
+          authenticatedUser: user,
+        };
+      });
+      // Set cookie
+      Cookies.set('authenticatedUser', JSON.stringify(user), { expires: 1 });
+    }
+    return user;
   }
 
   signOut = () => {
-
+    this.setState(() => {
+      return {
+        authenticatedUser: null,
+      };
+    });
+    Cookies.remove('authenticatedUser');
   }
 }
 
